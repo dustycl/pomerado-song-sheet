fbe61bdfac697f36059bc6d69eef44b2a1b7d1e7
Dustin Lamperts <dustycl@gmail.com>
2022-01-28 15:30:35 -0800

    null check service data before render

--- pages/services.js
+++ pages/services.js
@@ -12,7 +12,7 @@ export async function getStaticProps() {
 
 const SongList = ({ songs }) => {
     const songList = songs.map(({song}) => {
-        return <li>{song}</li>
+        return <li key={song}>{song}</li>
     })
     return <ul>{songList}</ul>
 }
@@ -21,9 +21,9 @@ const ServiceList = ({ servicesData }) => {
     const serviceList = servicesData.map(({ id, date, songs }) => {
         return (
             <li key={id}>
-                {date.toString()}
+                {date}
                 <br />
-                <SongList songs={songs} />
+                {songs && <SongList songs={songs} />}
             </li>
         )
     })
@@ -31,10 +31,22 @@ const ServiceList = ({ servicesData }) => {
 }
 
 export default function Services({ servicesData }) {
-    return (
-        <div className="container">
-            <h1>Services</h1>
-            <ServiceList servicesData={servicesData} />
-        </div>
-    )
+     // Check if servicesData is null
+    if (servicesData) {
+        return (
+            <div className="container">
+                <h1>Services</h1>
+                <ServiceList servicesData={servicesData} />
+            </div>
+        )
+    }
+    else{
+        return (
+            <div className="container">
+                <h1>Services</h1>
+                <p>It looks like you don't have any services yet</p>
+            </div>
+        )
+    }
+    
 }
---
+++ services/january-23rd.md
@@ -0,0 +1,5 @@
+---
+date: January 23rd
+songs:
+    - song: "anchor-for-the-soul"
+---
